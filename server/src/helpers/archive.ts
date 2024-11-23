0728a6f4b0ea14718ece890f24f6b66334e9e69b
burakbehlull <burakyabgu@gmail.com>
2024-11-24 02:33:21 +0300

    feat: Create archive helper class

---
+++ server/src/helpers/archive.ts
@@ -0,0 +1,37 @@
+import { Response } from "express"
+import archiver from "archiver"
+
+class Archive {
+    res: Response
+    archive: archiver.Archiver | null
+    constructor(res:Response){
+        this.archive = archiver('zip', {
+            zlib: { level: 9 }
+        })
+        this.res = res
+    }
+    setHeaders(fileName: string){
+        this.res.setHeader('Content-Disposition', `attachment; filename=${fileName}.zip`)
+        this.res.setHeader('Content-Type', 'application/zip')
+    }
+    createArchive(){
+        this.archive?.pipe(this.res)
+        return this.archive
+    }
+    add(content: string, fileName: string){
+        this.archive?.append(content, { name: fileName })
+    }
+    done(){
+        this.archive?.finalize()
+    }
+    watchError(){
+        this.archive?.on('error', (err: Error) => {
+            console.error('Zip oluşturulurken hata: ', err.message)
+        })
+    }
+
+}
+
+export default Archive
+
+
